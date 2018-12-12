eca56d5ae84157163aeabba60d695432ce469085
jhermann <jh@web.de>
2018-12-12 11:56:31 +0100

    call setuptools so --name etc works

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ project = dict(
     name = 'py-generic-project',
     version = '1.2',
     packages = [],
+    url='https://github.com/Springerle/py-minimal-script',
 )
+
+if __name__ == '__main__':
+    import setuptools
+    setuptools.setup(**project)
